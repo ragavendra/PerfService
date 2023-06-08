e613c5360eed100f70d864fabf8daf3911bf402a
Ragavendra <raga@example.com>
2023-06-08 11:33:16 -0700

    update to use

--- PerfRunner/Models/UserFormat.cs
+++ PerfRunner/Models/UserFormat.cs
@@ -7,13 +7,22 @@ namespace PerfRunner.Models
 {
     public class UserFormatInfo
    {
+      private readonly string _userAccountFormat =  "abc_{0:D7}@somecompany.co";
+
+      private readonly long _totalUsers = 1000;
+
+      private readonly long _userStartIndex = 6000;
+
       // account format for UserSource.Format user type
-      public string UserAccountFormat { get; set; } = "abc_{0:D7}@somecompany.co";
+      public string UserAccountFormat
+      {
+         get { return _userAccountFormat; }
+      }
 
       // total users for ....Format 
-      public long TotalUsers { get; set; } = 1000;
+      public long TotalUsers { get { return _totalUsers; } }
 
       // users start index for ....Format 
-      public long UserStartIndex { get; set; } = 6000;
+      public long UserStartIndex { get { return _userStartIndex; } }
    }
 }
